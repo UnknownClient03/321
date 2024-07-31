0cbcd0d3b73175b64828c499fc2efb08ce9afc01
BigGuyKurt <kurtisboon03@gmail.com>
2024-08-01 05:11:31 +1000

    Added UsefulContacts.java and ParentInfo.java
    Added UsefulContacts.java and ParentInfo.java

---
+++ app/src/main/java/com/example/myapplication/UsefulContacts.java
@@ -0,0 +1,13 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UsefulContacts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.useful_contacts);
+    }
+}
