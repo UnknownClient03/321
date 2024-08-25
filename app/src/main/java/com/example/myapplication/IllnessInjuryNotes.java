0179e865fa95a241be4e4bf8575fcb587169ed8e
UnknownClient03 <68719672+UnknownClient03@users.noreply.github.com>
2024-08-26 00:17:17 +1000

    Adding Activities for the records part
    Adding Activities for the records part

---
+++ app/src/main/java/com/example/myapplication/IllnessInjuryNotes.java
@@ -0,0 +1,31 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class IllnessInjuryNotes extends AppCompatActivity {
+
+    public LoginManager manager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_illness_injury_notes);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+
+        });
+
+        navBarManager.setNavBarButtons(IllnessInjuryNotes.this);
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) manager = new LoginManager(extras.getInt("guardianID"), extras.getInt("childID"));
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/Records.java
@@ -0,0 +1,63 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Records extends AppCompatActivity {
+    public LoginManager manager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_records);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        navBarManager.setNavBarButtons(Records.this);
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) manager = new LoginManager(extras.getInt("guardianID"), extras.getInt("childID"));
+
+        Button button = findViewById(R.id.button_myRecords_1);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.d("BUTTON", "changing to Progress Notes");
+                Intent intent=new Intent(Records.this, ProgressNotes.class);
+                Bundle extras = getIntent().getExtras();
+                if (extras != null) {
+                    intent.putExtra("guardianID", extras.getInt("guardianID"));
+                    intent.putExtra("childID", extras.getInt("childID"));
+                }
+                startActivity(intent);
+            }
+        });
+
+
+        button = findViewById(R.id.button_myRecords_2);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.d("BUTTON", "changing to Illness and Injury Notes");
+                Intent intent=new Intent(Records.this, IllnessInjuryNotes.class);
+                Bundle extras = getIntent().getExtras();
+                if (extras != null) {
+                    intent.putExtra("guardianID", extras.getInt("guardianID"));
+                    intent.putExtra("childID", extras.getInt("childID"));
+                }
+                startActivity(intent);
+            }
+        });
+
+    }
+}
