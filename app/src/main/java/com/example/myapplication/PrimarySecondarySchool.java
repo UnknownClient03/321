e4d17d2f90b3926b534f018c7ffd0fb6e9e9a459
BigGuyKurt <kurtisboon03@gmail.com>
2024-09-13 22:26:33 +1000

    Added PrimarySecondarySchool.java
    Added PrimarySecondarySchool.java

---
+++ app/src/main/java/com/example/myapplication/PrimarySecondarySchool.java
@@ -0,0 +1,31 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.widget.TextView;
+
+public class PrimarySecondarySchool extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_primary_secondary_school);
+
+        // Apply system window insets to the main view
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Set the text for the TextView with clickable links
+        TextView healthyEatingText = findViewById(R.id.healthyEatingText);
+        healthyEatingText.setText(Html.fromHtml(getString(R.string.healthy_eating_text), Html.FROM_HTML_MODE_LEGACY));
+        healthyEatingText.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+}
