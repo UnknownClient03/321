e4275f35dbb50eb16085b9920f7cc0dec3c2736e
TomKaro3 <144401994+TomKaro3@users.noreply.github.com>
2024-09-24 23:31:17 +1000

    fixed cpr chart

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,4 +39,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.github.chrisbanes:PhotoView:2.3.0")
 }
