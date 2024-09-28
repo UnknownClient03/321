46409ed03520cbd79230bf296b005a9e71266d66
BigJ742 <jtzitziris@gmail.com>
2024-09-28 16:35:46 +1000

    Update SQLConnection.java

--- app/src/main/java/com/example/myapplication/SQLConnection.java
+++ app/src/main/java/com/example/myapplication/SQLConnection.java
@@ -15,6 +15,9 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 import java.util.Arrays;
 import java.util.Base64;
