223b712bc9ee46d01786da6cbf78d666515721da
UnknownClient03 <68719672+UnknownClient03@users.noreply.github.com>
2024-10-04 15:10:23 +1000

    fixing up navBarManager in IllnessInjuryNotes.java

--- app/src/main/java/com/example/myapplication/IllnessInjuryNotes.java
+++ app/src/main/java/com/example/myapplication/IllnessInjuryNotes.java
@@ -24,8 +24,8 @@ public class IllnessInjuryNotes extends AppCompatActivity {
 
         });
 
-        NavBarManager.setNavBarButtons(IllnessInjuryNotes.this);
-        Bundle extras = getIntent().getExtras();
+                Bundle extras = getIntent().getExtras();
         if (extras != null) manager = new LoginManager(extras.getInt("guardianID"), extras.getInt("childID"));
+        NavBarManager.setNavBarButtons(IllnessInjuryNotes.this, manager);
     }
 }
