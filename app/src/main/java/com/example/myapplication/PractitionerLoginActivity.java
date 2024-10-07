39139aaf614e7f8eaf30c7ae392e3fe998af330f
UnknownClient03 <68719672+UnknownClient03@users.noreply.github.com>
2024-10-08 02:02:47 +1100

    Added in proper functionality for PractitionerLogin.java

--- app/src/main/java/com/example/myapplication/PractitionerLoginActivity.java
+++ app/src/main/java/com/example/myapplication/PractitionerLoginActivity.java
@@ -16,6 +16,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.HashMap;
+
 public class PractitionerLoginActivity extends AppCompatActivity {
 
     private EditText editTextPassword;
@@ -35,30 +37,20 @@ public class PractitionerLoginActivity extends AppCompatActivity {
             return insets;
         });
 
-        editTextPassword = findViewById(R.id.editTextTextPassword2);
+        EditText passwordView = findViewById(R.id.editTextTextPassword2);
+        EditText emailView = findViewById(R.id.editTextTextEmailAddress2);
         loginButton = findViewById(R.id.login_button);
         loginButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                if (validatePassword()) {
+                int pracID = validateLogin(passwordView.getText().toString(), emailView.getText().toString());
+                if (pracID != -1) {
                     Log.d("BUTTON", "Changing to select child page");
                     Intent intent = new Intent(PractitionerLoginActivity.this, ChildSelectActivity.class);
-                    intent.putExtra("guardianID", 0);
-                    intent.putExtra("childID", 0);
+                    intent.putExtra("pracID", pracID);
                     startActivity(intent);
                 } else {
                     Toast.makeText(PractitionerLoginActivity.this, "Please enter a valid password", Toast.LENGTH_LONG).show();
                 }
-            }
-        });
-
-        signupButton = findViewById(R.id.signup_button);
-        signupButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Log.d("BUTTON", "Changing to sign up");
-                Intent intent=new Intent(PractitionerLoginActivity.this, SignUpActivity.class);
-                intent.putExtra("guardianID", 0);
-                intent.putExtra("childID", 0);
-                startActivity(intent);
             }
         });
 
@@ -99,16 +91,31 @@ public class PractitionerLoginActivity extends AppCompatActivity {
         });
     }
 
-    private boolean validatePassword() {
-        String password = editTextPassword.getText().toString().trim();
-
-        if (password.isEmpty() || password.length() <= 5) {
-            return false;
+    private int validateLogin(String pass, String email)
+    {
+        SQLConnection conn = new SQLConnection("user1", "");
+        if(!conn.isConn()) return -1;
+        String query = "SELECT salt, pepper FROM PractitionerAccountDetails INNER JOIN Practitioner ON practitionerID = ID where email = ?;";
+        HashMap<String, String[]> result = conn.select(query, new String[]{email}, new char[]{'s'});
+        if(result.get("salt").length == 0)
+        {
+            conn.disconnect();
+            return -1;
         }
-
-        boolean hasUppercase = !password.equals(password.toLowerCase());
-        boolean hasSpecial = password.matches(".*[!@#$%^&*()_+=<>?{}\\[\\]~-].*");
-
-        return hasUppercase && hasSpecial;
+        String salt = result.get("salt")[0];
+        String pepper = result.get("pepper")[0];
+        String password = salt + pass + pepper;
+        Log.d("MSG", password);
+        String hash = SHA256.convert(password);
+        Log.d("MSG", hash);
+        int ID = -1;
+        query = "SELECT ID FROM Practitioner INNER JOIN PractitionerAccountDetails ON ID = practitionerID where email = ? AND Hashpassword = ?;";
+        result = conn.select(query, new String[]{email, hash}, new char[]{'s', 's'});
+        if(result.get("ID").length > 0)
+        {
+            ID = Integer.parseInt(result.get("ID")[0]);
+        }
+        conn.disconnect();
+        return ID;
     }
 }
